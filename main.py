af447e2d3fadfa5fe964e8f05be2ea8a193180d2
Vlad-cm <vladprmk@gmail.com>
2021-04-04 19:38:50 +0300

    Fixed password generation algorithm

--- main.py
+++ main.py
@@ -7,43 +7,40 @@ lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
 uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 punctuation = '!#$%&*+-=?@^_.'
 
-
-print('Сколько паролей вам нужно?')
-quantity = int(input())
-print('Насколько длинный должен быть ваш пароль?')
-length = int(input())
+password_list = []
+quantity = int(input('Сколько паролей вам нужно?'))
+length = int(input('Насколько длинный должен быть ваш пароль?'))
 print('Включать ли цифры?[yes/no]')
 answer_digits = input()
+if answer_digits == "yes":
+    for a in digits:
+        password_list.append(a)
 print('Включать ли маленькие буквы?[yes/no]')
 answer_lowercase = input()
+if answer_lowercase == "yes":
+    for b in lowercase_letters:
+        password_list.append(b)
 print('Включать ли большие буквы?[yes/no]')
 answer_uppercase = input()
+if answer_uppercase == "yes":
+    for c in uppercase_letters:
+        password_list.append(c)
 print('Включать ли знаки пунктуации?[yes/no]')
 answer_punctuation = input()
+if answer_punctuation == "yes":
+    for d in punctuation:
+        password_list.append(d)
 
 
-def generate_password(num):
-    password_list = []
-    for j in range(num):
-        password = ''
-        for i in range(length):
-            if answer_lowercase == 'yes' or answer_lowercase == '1':
-                password+=(lowercase_letters[random.randint(0, len(lowercase_letters)-1)])
-            if answer_uppercase == 'yes' or answer_uppercase == '1':
-                password+=(uppercase_letters[random.randint(0, len(uppercase_letters)-1)])
-            if answer_digits == 'yes' or answer_digits == '1':
-                password+=(digits[random.randint(0, len(digits)-1)])
-            if answer_punctuation == 'yes' or answer_punctuation == '1':
-                password+=(punctuation[random.randint(0, len(punctuation)-1)])
-        password_list.append(password)
-
-    return password_list
+def generate_password(len, count):
+    password = []
+    for i in range(count):
+        temp = []
+        for j in range(len):
+            temp.append(random.choice(password_list))
+        password.append(temp)
+    return password
 
 
-print(*generate_password(quantity))
-
-
-
-
-
-
+for passw in generate_password(length, quantity):
+    print(*passw, sep="")
